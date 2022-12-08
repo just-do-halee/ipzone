cbf023b3beebc1c74cdb949e4c4ad211147fdb0b
just-do-halee <just.do.halee@gmail.com>
2022-12-08 13:30:18 +0900

    Remove global! macro

--- src/global.rs
+++ src/global.rs
@@ -2,13 +2,6 @@ use once_cell::sync::Lazy;
 
 pub type Global<T> = Lazy<T>;
 
-#[macro_export]
-macro_rules! global {
-    ($e:expr) => {
-        Global::new(|| $e);
-    };
-}
-
 #[inline]
 pub const fn global<T>(f: fn() -> T) -> Global<T> {
     Global::new(f)
